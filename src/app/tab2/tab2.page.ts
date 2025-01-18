0ec0eb4c529526537cc244201956274fde4b94ee
Vojtech <vojtechvasina@email.cz>
2025-01-19 00:20:19 +0100

    Pridana tab pro scan

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, runInInjectionContext } from '@angular/core';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-tab2',
@@ -7,7 +8,21 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab2Page {
+  scanResult = '';
+  isScanning = false;
 
   constructor() {}
 
+  async scanBarcode() {
+    this.isScanning = true;
+    const status = await BarcodeScanner.checkPermission({ force: true });
+
+    if (status.granted) {
+      const result = await BarcodeScanner.startScan();
+      if (result.hasContent) {
+        this.scanResult = result.content;
+      }
+    }
+    this.isScanning = false;
+  }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,12 @@
 import { Component, runInInjectionContext } from '@angular/core';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss'],
   standalone: false,
 })
-export class Tab1Page {
+export class Tab3Page {
 
   constructor() {}
 }
