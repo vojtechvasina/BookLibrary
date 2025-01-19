039b15781277c7659b0304b5243b8acba4051c60
Vojtech <vojtechvasina@email.cz>
2025-01-19 12:03:24 +0100

    Pridani Google Books API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { provideHttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideHttpClient()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component, runInInjectionContext } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,32 @@ import { Component, runInInjectionContext } from '@angular/core';
   standalone: false,
 })
 export class Tab1Page {
+  searchTerm: string = '';
+  books: any[] = [];
+  isLoading: boolean = false;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  async searchBooks() {
+    if (this.searchTerm) {
+      this.isLoading = true;
+      try {
+        const url = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(this.searchTerm)}&maxResults=10`;
+        
+        this.http.get(url).subscribe((data: any) => {
+          this.books = data.items || [];
+          this.isLoading = false;
+        });
+      } catch (error) {
+        console.error('Error fetching books:', error);
+        this.isLoading = false;
+      }
+    }
+  }
+
+  // Můžeš přidat metodu pro vyhledávání při psaní
+  onSearchChange(event: any) {
+    this.searchTerm = event.detail.value;
+    this.searchBooks();
+  }
 }
